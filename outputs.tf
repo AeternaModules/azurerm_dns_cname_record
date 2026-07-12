59b1b0a4fef11456fa5c352ed5fa627e76bb6aea
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:02:00 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "dns_cname_records_id" {
+  description = "Map of id values across all dns_cname_records, keyed the same as var.dns_cname_records"
+  value       = { for k, v in azurerm_dns_cname_record.dns_cname_records : k => v.id }
+}
 output "dns_cname_records_fqdn" {
   description = "Map of fqdn values across all dns_cname_records, keyed the same as var.dns_cname_records"
   value       = { for k, v in azurerm_dns_cname_record.dns_cname_records : k => v.fqdn }
