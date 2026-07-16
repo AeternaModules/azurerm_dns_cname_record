f37e98da4a7cec0dc7ad6d0a6f532f42c4b48f10
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:47:54 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,37 @@
 output "dns_cname_records_id" {
   description = "Map of id values across all dns_cname_records, keyed the same as var.dns_cname_records"
-  value       = { for k, v in azurerm_dns_cname_record.dns_cname_records : k => v.id }
+  value       = { for k, v in azurerm_dns_cname_record.dns_cname_records : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "dns_cname_records_fqdn" {
   description = "Map of fqdn values across all dns_cname_records, keyed the same as var.dns_cname_records"
-  value       = { for k, v in azurerm_dns_cname_record.dns_cname_records : k => v.fqdn }
+  value       = { for k, v in azurerm_dns_cname_record.dns_cname_records : k => v.fqdn if v.fqdn != null && length(v.fqdn) > 0 }
 }
 output "dns_cname_records_name" {
   description = "Map of name values across all dns_cname_records, keyed the same as var.dns_cname_records"
-  value       = { for k, v in azurerm_dns_cname_record.dns_cname_records : k => v.name }
+  value       = { for k, v in azurerm_dns_cname_record.dns_cname_records : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "dns_cname_records_record" {
   description = "Map of record values across all dns_cname_records, keyed the same as var.dns_cname_records"
-  value       = { for k, v in azurerm_dns_cname_record.dns_cname_records : k => v.record }
+  value       = { for k, v in azurerm_dns_cname_record.dns_cname_records : k => v.record if v.record != null && length(v.record) > 0 }
 }
 output "dns_cname_records_resource_group_name" {
   description = "Map of resource_group_name values across all dns_cname_records, keyed the same as var.dns_cname_records"
-  value       = { for k, v in azurerm_dns_cname_record.dns_cname_records : k => v.resource_group_name }
+  value       = { for k, v in azurerm_dns_cname_record.dns_cname_records : k => v.resource_group_name if v.resource_group_name != null && length(v.resource_group_name) > 0 }
 }
 output "dns_cname_records_tags" {
   description = "Map of tags values across all dns_cname_records, keyed the same as var.dns_cname_records"
-  value       = { for k, v in azurerm_dns_cname_record.dns_cname_records : k => v.tags }
+  value       = { for k, v in azurerm_dns_cname_record.dns_cname_records : k => v.tags if v.tags != null && length(v.tags) > 0 }
 }
 output "dns_cname_records_target_resource_id" {
   description = "Map of target_resource_id values across all dns_cname_records, keyed the same as var.dns_cname_records"
-  value       = { for k, v in azurerm_dns_cname_record.dns_cname_records : k => v.target_resource_id }
+  value       = { for k, v in azurerm_dns_cname_record.dns_cname_records : k => v.target_resource_id if v.target_resource_id != null && length(v.target_resource_id) > 0 }
 }
 output "dns_cname_records_ttl" {
   description = "Map of ttl values across all dns_cname_records, keyed the same as var.dns_cname_records"
-  value       = { for k, v in azurerm_dns_cname_record.dns_cname_records : k => v.ttl }
+  value       = { for k, v in azurerm_dns_cname_record.dns_cname_records : k => v.ttl if v.ttl != null }
 }
 output "dns_cname_records_zone_name" {
   description = "Map of zone_name values across all dns_cname_records, keyed the same as var.dns_cname_records"
-  value       = { for k, v in azurerm_dns_cname_record.dns_cname_records : k => v.zone_name }
+  value       = { for k, v in azurerm_dns_cname_record.dns_cname_records : k => v.zone_name if v.zone_name != null && length(v.zone_name) > 0 }
 }
 
